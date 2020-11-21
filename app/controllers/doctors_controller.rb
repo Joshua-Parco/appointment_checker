051a0462f19fc395a7a6f844e8395751b98ff9a7
Joshua Parco <jacparco@gmail.com>
2020-11-20 23:42:16 -0700

    views for users and new users

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,13 @@
 class DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 end
