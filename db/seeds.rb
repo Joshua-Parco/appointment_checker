01e5515970989b906d4983e165a91222faeb7618
Joshua Parco <jacparco@gmail.com>
2020-11-20 22:48:32 -0700

    models, controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+50.times do
+  user = User.create(name: Faker::FunnyName.name)
+end
+
+15.times do
+  doctor = Doctor.create(name: Faker::FunnyName.name)
+end
+
+75.times do
+  appointment = Appointment.create(user_id: User.all.sample.id, doctor_id: Doctor.all.sample.id, appointment_date: Faker::Time.forward(days: 120, format: :long))
+end
+
